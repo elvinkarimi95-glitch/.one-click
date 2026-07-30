4c4b6e29b129d4d0c714af7e43a050ba62861d52
elvinkarimi95-glitch <elvinkarimi95@gmail.com>
2026-07-30 22:00:19 +0330

    Update script.js

--- webapp/script.js
+++ webapp/script.js
@@ -1,1 +1,28 @@
+let score = 0;
 
+const coin = document.getElementById("coin");
+const scoreText = document.getElementById("score");
+
+// اگر قبلاً امتیاز ذخیره شده باشد
+if (localStorage.getItem("score")) {
+    score = Number(localStorage.getItem("score"));
+    scoreText.innerText = score;
+}
+
+coin.addEventListener("click", () => {
+
+    score++;
+
+    scoreText.innerText = score;
+
+    // ذخیره امتیاز
+    localStorage.setItem("score", score);
+
+    // انیمیشن کلیک
+    coin.style.transform = "scale(0.9)";
+
+    setTimeout(() => {
+        coin.style.transform = "scale(1)";
+    },100);
+
+});
